66374b59cf619e393a18b16239c984e60e8d534a
Juliano Vince De Campos <juliano.campos@mercadolivre.com>
2021-01-05 15:35:21 -0300

    Configuração do projeto

---
+++ app/src/main/java/com/br/julianovincedecampos/memorynotes/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.br.julianovincedecampos.memorynotes.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.br.julianovincedecampos.memorynotes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

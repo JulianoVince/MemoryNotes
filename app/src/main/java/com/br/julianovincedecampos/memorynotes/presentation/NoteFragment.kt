acca8893bfcb47ea9b7f62de96fb32c0888055e0
Juliano Vince De Campos <juliano.campos@mercadolivre.com>
2021-01-06 16:50:07 -0300

    Criando a implementação do ViewModel NoteFrament

--- app/src/main/java/com/br/julianovincedecampos/memorynotes/presentation/NoteFragment.kt
+++ app/src/main/java/com/br/julianovincedecampos/memorynotes/presentation/NoteFragment.kt
@@ -1,15 +1,26 @@
 package com.br.julianovincedecampos.memorynotes.presentation
 
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
+import com.br.julianovincedecampos.core.data.Note
 import com.br.julianovincedecampos.memorynotes.R
+import com.br.julianovincedecampos.memorynotes.framework.NoteViewModel
 import kotlinx.android.synthetic.main.fragment_note.*
 
 class NoteFragment : Fragment() {
+
+    private lateinit var viewModel: NoteViewModel
+    private var currentNote = Note("", "", 0L, 0L)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,7 +32,39 @@ class NoteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        checkButton.setOnClickListener { Navigation.findNavController(it).popBackStack() }
+
+        viewModel = ViewModelProviders.of(this).get(NoteViewModel::class.java)
+
+        checkButton.setOnClickListener {
+            if (titleView.text.toString() != "" || contentView.text.toString() != "") {
+                val time = System.currentTimeMillis()
+                currentNote.title = titleView.text.toString()
+                currentNote.content = contentView.text.toString()
+                currentNote.updateTine = time
+                if (currentNote.id == 0L) {
+                    currentNote.creationTime = time
+                }
+                viewModel.saveNote(currentNote)
+            }
+            Navigation.findNavController(it).popBackStack()
+        }
+        observeViewModel()
     }
 
+    private fun observeViewModel() {
+        viewModel.saved.observe(this, Observer {
+            if (it) {
+                Toast.makeText(context, "Done!", Toast.LENGTH_SHORT).show()
+                hideKeyboard()
+                Navigation.findNavController(titleView).popBackStack()
+            } else {
+                Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+    private fun hideKeyboard() {
+        val imm = context?.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(titleView.windowToken, 0)
+    }
 }
